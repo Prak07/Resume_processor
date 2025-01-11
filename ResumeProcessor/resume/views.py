f32caa2f1d57ffa4e8d6c1c478077d60d9216601
Prak07 <tarangagrawal@gmail.com>
2025-01-12 00:49:07 +0530

    changes made and added llm for parsing

--- ResumeProcessor/resume/views.py
+++ ResumeProcessor/resume/views.py
@@ -3,16 +3,12 @@ from rest_framework.response import Response
 import PyPDF2
 import docx
 import spacy
-from spacy.matcher import Matcher
-import re
 from django.core.files.storage import default_storage
 from .models import Candidate
 from .serializers import CandidateSerializer
 from groq import Groq   
-nlp = spacy.load("en_core_web_sm")
-# Regular expressions to find email and mobile number
-EMAIL_REGEX = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-MOBILE_REGEX = r'((?:\(?\+91\)?)?\d{10})'
+
+
 client = Groq(api_key="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")
 
 @api_view(['POST'])
@@ -75,37 +71,8 @@ def extract_resume(request):
         first_name=name.split()[0]
         email=result[1]
         mobile=result[2]
-        # data=content.split()
-        # # Use regular expressions to extract email and mobile number
-        # email = re.findall(EMAIL_REGEX, content)
-        # mobile = re.findall(MOBILE_REGEX, content)
-        # # Extract the first name (naive approach: take the first word from the resume)
-        # # Use spaCy's Named Entity Recognition (NER) to extract names
-        # doc = nlp(content)
-        # # matcher = Matcher(nlp.vocab)
 
-        # # # Define a pattern for names (Capitalized words)
-        # # pattern = [
-        # #     {"IS_UPPER": True},
-        # # ]
-
-        # # Add the pattern to the matcher
-        # # matcher.add("TITLE_OR_UPPER", [pattern])
-        # # first_name = data[0]
-        # # matches = matcher(doc)
-        # # Look for entities labeled as 'PERSON' (names)
-        
-        # for ent in doc.ents:
-        #     if ent.label_ == "PERSON":
-        #         first_name = ent.text.split()[0]  # Take the first name from the full name
-        #         break  # Stop after finding the first name
-        # # names=[]
-        # # for match_id, start, end in matches:
-        # #     names.append(doc[start:end].text)
-            
-        # # print(names)
-        # # first_name=names[0]
-        # # Return the extracted details
+        # Return the extracted details
         candidate = Candidate.objects.create(
             first_name=first_name if first_name else None,
             email=email.strip() if email else None,
